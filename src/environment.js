6d5c6d58d62b5879d95b15b9fff7f8c9e7dafd42
nodeexcel <vaibhav.excel2011@gmail.com>
2018-05-05 22:52:06 +0530

    config details

--- src/environment.js
+++ src/environment.js
@@ -1,14 +1,28 @@
 let config;
 let db;
-config = {
-    "port": 8091,
-    "bodyLimit": "100kb",
-    "corsHeaders": ["Link"],
-    "db": {
-        "host": "127.0.0.1",
-        "name": "nicholas",
-        "password": "java@123",
-        "username": "root"
+if (process.env.ENV == "dev") {
+    config = {
+        "port": 8091,
+        "bodyLimit": "100kb",
+        "corsHeaders": ["Link"],
+        "db": {
+            "host": "127.0.0.1",
+            "name": "nicholas",
+            "password": "java@123",
+            "username": "root"
+        }
+    }
+} else {
+    config = {
+        "port": 8091,
+        "bodyLimit": "100kb",
+        "corsHeaders": ["Link"],
+        "db": {
+            "host": "nicholas.cs26dooygdod.us-east-2.rds.amazonaws.com",
+            "name": "nicholas",
+            "password": "java@123",
+            "username": "root"
+        }
     }
 }
 export default {
