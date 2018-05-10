eb5ef71c98a8265d1cc9c25fe3ed591a6e0148f9
nodeexcel <vaibhav.excel2011@gmail.com>
2018-05-10 14:58:06 +0530

    updated

--- src/controllers/imageProcess.js
+++ src/controllers/imageProcess.js
@@ -25,7 +25,7 @@ export class UserController extends BaseAPIController {
         form.parse(req, function(err, fields, files) {
 
             if (files.file) {
-                if (files.file.type != 'application/zip') {
+                if (files.file.type != 'application/zip' || files.file.type != 'application/x-zip-compressed') {
                     res.status(400).json({ error: 1, message: "please upload zip file" })
                 } else {
                     fs.readFile(files.file.path, function(err, data) {
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -12,53 +12,55 @@ export class UserController extends BaseAPIController {
     createProducts = (req, res, next) => {
         let form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
-            if (files && files.file.type != 'text/csv') {
-                res.status(400).json({ error: 1, message: "please upload csv file" })
+            if (files.file) {
+                if (files.file['name'].substr(files.file['name'].lastIndexOf('.') + 1).toLowerCase() != 'csv') {
+                    res.status(400).json({ error: 1, message: "please upload csv file" })
+                } else {
+                    fs.readFile(files.file.path, function(err, data) {
+                        var csv = data.toString('utf8')
+                        var lines = csv.split("\n");
+                        var result = [];
+                        var headers = lines[0].split(",");
+                        for (var i = 1; i < lines.length; i++) {
+                            var obj = {};
+                            var currentline = lines[i].split(/,|"/);
+                            for (var j = 0; j < headers.length; j++) {
+                                obj[headers[j]] = currentline[j] ? currentline[j] : null;
+                            }
+                            result.push(obj);
+                        }
+
+                        db.products.findAll({}).then((resp) => {
+                            var arrToDelete = []
+                            _.map(resp, (val, key) => {
+                                let findProduct = _.find(result, function(get) { return get.ProductID == val.ProductID; });
+                                if (!findProduct) {
+                                    arrToDelete.push(val.ProductID)
+                                }
+                            })
+                            db.products.destroy({ where: { ProductID: arrToDelete } }).then((data) => {
+                                console.log(data, "deleted")
+                            })
+                        })
+
+                        _.forEach(result, (val, key) => {
+                            delete val.ImageFullsizeURL;
+                            db.products.findOne({ where: { ProductID: val.ProductID } }).then((product) => {
+                                if (product) {
+                                    db.products.update(val, { where: { ProductID: val.ProductID } }).then((data) => {})
+                                } else {
+                                    db.products.create(val).then((data) => {})
+                                }
+                            })
+                            if (key == result.length - 1) {
+                                res.json({ status: 1, message: "success" })
+                            }
+                        })
+                    })
+                }
             } else {
-                fs.readFile(files.file.path, function(err, data) {
-                    var csv = data.toString('utf8')
-                    var lines = csv.split("\n");
-                    var result = [];
-                    var headers = lines[0].split(",");
-                    for (var i = 1; i < lines.length; i++) {
-                        var obj = {};
-                        var currentline = lines[i].split(/,|"/);
-                        for (var j = 0; j < headers.length; j++) {
-                            obj[headers[j]] = currentline[j] ? currentline[j] : null;
-                        }
-                        result.push(obj);
-                    }
-
-                    db.products.findAll({}).then((resp) => {
-                        var arrToDelete = []
-                        _.map(resp, (val, key) => {
-                            console.log(resp.length, result.length, val.ProductID)
-                            let findProduct = _.find(result, function(get) { return get.ProductID == val.ProductID; });
-                            if (!findProduct) {
-                                arrToDelete.push(val.ProductID)
-                            }
-                        })
-                        db.products.destroy({ where: { ProductID: arrToDelete } }).then((data) => {
-                            console.log(data, "deleted")
-                        })
-                    })
-
-                    _.forEach(result, (val, key) => {
-                        delete val.ImageFullsizeURL;
-                        db.products.findOne({ where: { ProductID: val.ProductID } }).then((product) => {
-                            if (product) {
-                                db.products.update(val, { where: { ProductID: val.ProductID } }).then((data) => {})
-                            } else {
-                                db.products.create(val).then((data) => {})
-                            }
-                        })
-                        if (key == result.length - 1) {
-                            res.json({ status: 1, message: "success" })
-                        }
-                    })
-                })
+                res.status(400).json({ error: 1, message: "file not recieved" })
             }
-
         })
     }
 
