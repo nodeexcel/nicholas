5ddac714d3d22cddfa0cf3bd3bc055c1256eeace
nodeexcel <vaibhav.excel2011@gmail.com>
2018-05-10 15:20:38 +0530

    updated

--- src/controllers/imageProcess.js
+++ src/controllers/imageProcess.js
@@ -25,7 +25,7 @@ export class UserController extends BaseAPIController {
         form.parse(req, function(err, fields, files) {
 
             if (files.file) {
-                if (files.file.type != 'application/zip' || files.file.type != 'application/x-zip-compressed') {
+                if (files.file['name'].substr(files.file['name'].lastIndexOf('.') + 1).toLowerCase() != 'zip') {
                     res.status(400).json({ error: 1, message: "please upload zip file" })
                 } else {
                     fs.readFile(files.file.path, function(err, data) {
